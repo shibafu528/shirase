22ad26ad1d50d1e2fe53489254d3c6c4ce4585ca
shibafu <shibafu528@gmail.com>
2023-07-21 08:41:04 +0900

    return 405 in inbox
    currently not implemented yet.

--- handler/activitypub/inbox.go
+++ handler/activitypub/inbox.go
@@ -7,6 +7,6 @@ import (
 
 func PostInboxHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(501)
-	w.Write([]byte(fmt.Sprintf("{\"status\": 501, \"error\": \"NOT_IMPLEMENTED\", \"message\": \"%s\"}", "endpoint not implemented")))
+	w.WriteHeader(405)
+	w.Write([]byte(fmt.Sprintf("{\"status\": 405, \"error\": \"NOT_IMPLEMENTED\", \"message\": \"%s\"}", "endpoint not implemented")))
 }
