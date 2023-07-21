edeb79a434962d344e57d90a06c531e5051ff715
shibafu <shibafu528@gmail.com>
2023-07-22 03:25:05 +0900

    router: 最初から細かく分けても見づらいだけや...

--- router/router.go
+++ router/router.go
@@ -3,6 +3,8 @@ package router
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/shibafu528/shirase/handler/activitypub"
+	"github.com/shibafu528/shirase/handler/wellknown"
 )
 
 func New() *chi.Mux {
@@ -11,8 +13,15 @@ func New() *chi.Mux {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Logger)
 
-	r.Group(ActivityPub)
-	r.Group(WellKnown)
+	r.Route("/.well-known", func(r chi.Router) {
+		r.Get("/host-meta", wellknown.HostMetaHandler)
+		r.Get("/webfinger", wellknown.WebFingerHandler)
+	})
+	r.Route("/users/{username}", func(r chi.Router) {
+		r.Get("/", activitypub.GetPersonHandler)
+		r.Post("/inbox", activitypub.PostInboxHandler)
+		r.Get("/outbox", activitypub.GetOutboxHandler)
+	})
 
 	return r
 }
